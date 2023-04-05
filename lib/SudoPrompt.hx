c8f55b1a7b2a332ae853577e92853158674510ba
Dizbdeedee <dizbee@mentice.cotse.net>
2023-04-05 18:04:35 +0000

    Missing lib files
    FossilOrigin-Name: 8e4b3ff617f29a34d86ae37ee1717d64a38af321e7077a2be4d5f0d49d720849

---
+++ lib/SudoPrompt.hx
@@ -0,0 +1,3 @@
+@:jsRequire("sudo-prompt") @valueModuleOnly extern class SudoPrompt {
+	static function exec(cmd:String, ?options:ts.AnyOf5<() -> Void, (error:js.lib.Error) -> Void, (error:js.lib.Error, stdout:Dynamic) -> Void, (error:js.lib.Error, stdout:Dynamic, stderr:Dynamic) -> Void, { @:optional var name : String; @:optional var icns : String; @:optional var env : { }; }>, ?callback:ts.AnyOf4<() -> Void, (error:js.lib.Error) -> Void, (error:js.lib.Error, stdout:Dynamic) -> Void, (error:js.lib.Error, stdout:Dynamic, stderr:Dynamic) -> Void>):Void;
+}
